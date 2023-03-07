4863a6a953513528442de9f040953488923ad489
Renge <renge@renge.io>
2023-03-07 17:04:51 -0500

    init: first commit

---
+++ backend/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port: Number = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
+
