54f8de5d74171cbbfb2395bad428a10e38cc8b91
Chenxi0106 <chenxi.lin@stonybrook.edu>
2023-03-21 16:52:43 -0400

    fix

--- frontend/src/auth/auth-request-api/index.js
+++ frontend/src/auth/auth-request-api/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 axios.defaults.withCredentials = true;
-const api = axios.create({
-    baseURL: 'http://localhost',
-})
+const api = axios.create()
 const getLoggedIn = ()=> api.get(`/auth/loggedIn`);
 const isGlobalDeveloper=()=>api.get('/auth/globaldeveloper');
 const apis = {
